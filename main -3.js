e671fb65ed133dd3d7060ebbdd50dfdf27b4656a
Prabhnoor132010 <79254675+Prabhnoor132010@users.noreply.github.com>
2023-03-26 12:59:36 +0530

    Add files via upload

---
+++ main -3.js
@@ -0,0 +1,55 @@
+img="";
+object=[];
+status="";
+
+function preload() {
+    img= loadImage("three.jpg");
+}
+
+function setup() {
+    canvas=createCanvas(200,300);
+    canvas.position(450,150);
+
+    objectdetect=ml5.objectDetector("cocossd",modelLoaded);
+    document.getElementById("status").innerHTML="Status: Object Dectecting";
+}
+
+
+
+function modelLoaded() {
+    console.log("LOADED");
+    status=true;
+    objectdetect.detect(img,gotresults);
+}
+
+
+
+
+function gotresults(error,results) {
+    if (error) {
+        console.log(error)
+    } else {
+       console.log(results);
+       object=results 
+    }
+}
+
+function draw() {
+    image(img , 0 , 0 , 200 , 300);
+
+    if (status != "" ) {
+        for (i = 0; i < object.length; i++) {
+                document.getElementById("status").innerHTML="Status: Object Dectected";
+
+                per= Math.floor (object[i].confidence * 100) ;
+                text( object[i].label + " " + per + "%" , object[i].x+15 , object[i].y+15);
+                noFill();
+
+                stroke("blue");
+                rect(object[i].x , object[i].y , object[i].width , object[i].height);
+            
+        }
+    }
+}
+
+
---
+++ main.js
@@ -0,0 +1,21 @@
+
+function one() {
+    window.location="index -1.html"; 
+}
+
+function two() {
+    window.location="index -2.html"; 
+}
+
+function three() {
+    window.location="index -3.html"; 
+}
+
+function four() {
+    window.location="index -4.html"; 
+}
+
+function five() {
+    window.location="index -5.html"; 
+}
+
